81121825137832bf7a614c458bf9e86f157cd468
denisturea <tureadenis2@gmail.com>
2022-05-31 21:25:13 +0300

    commit message

---
+++ Modul1/part1.py
@@ -0,0 +1,20 @@
+# print('###', '# #', '###', sep= '\n')
+#
+# print('print')
+#
+# print('3')
+# print(type(3))
+
+# print(3+3)
+# print("3"+"3")
+#
+# print(3*3)
+# print("3"*"3")
+
+#print("#" * 30)
+#print("#", " " * 28, "#", sep='')
+#print("#" * 30)
+
+my_value = "#"* 30
+my_value1 = "#" + ''*26 + "#"
+print(my_value,my_value1,my_value,sep='\n')
