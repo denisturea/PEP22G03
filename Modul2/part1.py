f46893e849940325b05ea4802832476bbcaedc61
denisturea <tureadenis2@gmail.com>
2022-06-02 21:57:46 +0300

    commit message

---
+++ Modul2/part1.py
@@ -0,0 +1,22 @@
+#INT
+
+number  = 3
+print(type(number))
+number = int('3')
+print('Nr as text',type(number))
+#float
+
+number = 3.1
+print(type(number))
+
+# bool
+
+# True_ = 3
+bool_object = False
+print(type(bool_object))
+print("Adress is:",id(bool_object))
+print("Adress is:",id(number))
+# print("Adress is:",id(number1))
+
+print('COnvert to bool:', bool(0))
+print('COnvert to bool:', int(True))
---
+++ Modul2/part2lab.py
@@ -0,0 +1,50 @@
+# print('math')
+# b=2
+# h=3
+# A=1/2*b*h
+# print(A)
+
+#INPUT
+# b=int(input('Baza:'))
+# h=int(input('Inaltimea:'))
+#
+# A=1/2*b*h
+# print('Aria este:',A)
+# print(type(A))
+
+#prob 2
+# parola=int(input('Introduceti parola:'))
+# parola_salvata=7710
+#
+# if parola==parola_salvata:
+#     print('True')
+# else
+#     print('False')
+
+#prob 2
+# numar1 = int(input('Primul numar:'))
+# numar2 = int(input('Al doilea numar:'))
+#
+# print('Media numerelor este:',(numar1+numar2)/2)
+# print('Impartirea numerelor:',int(numar1/numar2))
+# print('ridicarea la putere:',numar1**numar2)
+# print('Media numerelor este:',(numar1+numar2)/2,'Impartirea numerelor:',int(numar1/numar2),'ridicarea la putere:',numar1**numar2)
+
+# venit = int(input("Venitul pe o lunae este:"))
+# a= 50/100*venit
+# b= 30/100*venit
+# c= 20/100*venit
+#
+# print( ' Venituri acordate necesitatiilor:',a,'\n','Venituri acordate scopuri recreationale:',b,'\n','Venituri acordate datoriilor si economiilor:',c,'\n')
+
+#numere complexe
+# my_number = 1 +4j
+# print(type(my_number))
+# my_number = (-1)**(1/2)
+# print(my_number)
+
+# a=3
+# b=4
+# c=5
+# print('rezultatul este:', (-b+((b**2-4*a*c)**(1/2)))/(2*a))
+
