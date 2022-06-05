cdd76129c126953f3b2c9eac3283431cbd52d765
denisturea <tureadenis2@gmail.com>
2022-06-05 16:04:35 +0300

    commit message

---
+++ Homework/HomeWorkM2.py
@@ -0,0 +1,34 @@
+# Ex1
+# pasw= input("Introduceti parola:")
+# paswc = "Passme1n"
+# print('Este corecta parola:',pasw==paswc)
+
+#Ex2 + Ex3
+# nume = input('Primul nume este:')
+# numar =int(input('Numarul este:'))
+# nume1 = input('Al doilea nume este:')
+# print('Exista numele?:',nume==nume1,'\n')
+# print('Este mai lung primul nume',len(nume)>len(nume1),'\n')
+# print('Initiala Primului nume:',nume[0])
+# print('Primul nume inversat este:',nume[::-1])
+# print('Rezultatul este:',nume*numar)
+
+#Ex4
+# var= 'Ananas'
+#
+# print(var[0],var[1],var[2],var[3],var[4],var[5],sep='\n')
+# print(var[0:3],var[3:6],sep='\n')
+# print(var[0:2],var[2:5],var[5],sep=":")
+# print(var[0:3],var[3:5],var[5],sep="_")
+# print(var[1:3]*8)
+
+#Suplimentar EX1
+
+# nume = str.upper(input("Numele este:\n"))
+# print(nume==nume[::-1])
+
+# Suplimentar Ex2
+
+# cuv=input('Cuvantul este:')
+# cuv1=str.upper(cuv[0])
+# print(cuv[0]==cuv1)
