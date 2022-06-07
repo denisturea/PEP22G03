9a6b58a8d08e4234f981cc5e27ade9df1d0b696b
denisturea <tureadenis2@gmail.com>
2022-06-07 21:21:47 +0300

    commit message

---
+++ Modul2/par23lab.py
@@ -0,0 +1,55 @@
+# part 2.3
+
+#len()function
+
+# var = "Hello Python"
+#
+# print("Lenght is:",len(var))
+# print("Lenght is:",var.__len__())
+#
+# #Normal string
+# normal = "Normal string \n"
+# normal1 = 'Normal \n string'
+
+#Multiline string
+
+# normal1= """Normal string
+# New Line"""
+# print(normal1)
+
+#raw string
+
+# x=r"""Normal sting \n
+# New line"""
+# print(x)
+
+#formated string
+# var1= 'insert'
+# var= f"""Normal string {var1}
+# New Line"""
+# print(var)
+
+# Ex1
+#
+# x=r"""Azi ma duc la  "facultate" """
+# print(x)
+# y=r"""/*\/*\*/*\/*\
+# Python
+# \./\./\./\./"""
+# print(y)
+# z=r"""P     y     t     o     n"""
+# print(z)
+# # Ex2
+
+
+# var1= 2022
+# q1=input("Cum te numesti:")
+# q2=int(input('Ce varsta ai:'))
+# x=f"""Ceau ana, deci te-ai nascut in anul: {var1-q2}"""
+#
+# print(x)
+
+#repalce method
+var1= "insert"
+var1= var1.replace('se','')
+print('Replaced i:', var1)
---
+++ Modul2/part22lab.py
@@ -0,0 +1,29 @@
+# var= "My python3 Course"
+#slice with single my value
+# print(var)
+# print(var[1])
+#slice with start and stop value
+#print(var[3:9])
+#slice with start stop and value
+#print(var[3:9:1])
+#print(var[3:9:2])
+#printul intreg fara start value
+#print(var[-1:-5:-1])
+
+# prob6:
+# var1="Ana are mere"
+# var2='Hello Python'
+# var3='Pizza Party'
+# print(var2[0:5],var2[6:],sep="_")
+# print(var1[:],end='.\n')
+# print(var3[:5]*4)
+
+# prob7
+
+# a=5.
+# b=5
+# c="ana"
+#
+# print(hex(id(a)),type(a))
+# print(hex(id(b)),type(b))
+# print(hex(id(c)),type(c))
