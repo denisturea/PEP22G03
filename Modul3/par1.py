2607f067c2c2bd547401a5fc41fc0f7e198d914f
denisturea <tureadenis2@gmail.com>
2022-06-14 21:40:42 +0300

    commit message

--- Modul3/par1.py
+++ Modul3/par1.py
@@ -70,56 +70,70 @@
 # print('Done')
 
 #Problema 3
-optiuni = input('Cappucino......4 lei\nExpreso......3.5lei\nCe optiune alegeti?:[1,2]:')
-bani = int(input("Introduceti bancnota [5,10]:"))
-var1= '1'
-var2= '2'
-ban = 5
-ban1 = 10
-pret1 =4
-pret2 = 3.5
-if optiuni == var1 and bani == (ban or ban1) :
+# optiuni = input('Cappucino......4 lei\nExpreso......3.5lei\nCe optiune alegeti?:[1,2]:')
+# bani = int(input("Introduceti bancnota [5,10]:"))
+# var1= '1'
+# var2= '2'
+# ban = 5
+# ban1 = 10
+# pret1 =4
+# pret2 = 3.5
+# if optiuni == var1 and bani == (ban or ban1) :
+#
+#     print("Vei primi restul de:",bani- pret1 )
+# elif optiuni== var2 and bani == (ban or ban1):
+#
+#     print("Vei primi restul de:",bani- pret2 )
+# # elif optiuni != (var1 and var2):
+# #     print("Alegere gresita!")
+# else:
+#     print('Alegere gresita!')
+#
+# #Problema3a
+#
+# print("1. Cappuccino     ... 4 lei","2. Espresso       ... 3.5 lei",sep="\n")
+# Cappucino = 4
+# Espresso = 3.5
+# optiune = 0
+# while not optiune:
+#     optiune = int(input("Ce optiune alegeti? [1,2]: "))
+#     if optiune == 1:
+#         print("Ati ales optiunea 1")
+#     elif optiune == 2:
+#         print("Ati ales optiunea 2")
+#     else:
+#         print("Alegere gresita")
+#         optiune = 0
+#
+# while True:
+#     bani = int(input("Introduceti o banconta [5,10]: "))
+#     if bani == 5:
+#         print("Ati introdus 5 lei")
+#         break
+#     elif bani == 10:
+#         print("Ati introdus 10 lei")
+#         break
+#     else:
+#         print("Se permit doar bancnote de 5 sau 10 lei")
+#         continue
+#
+# if optiune == 1:
+#     print(f"Veti primi restul de {bani-Cappucino} lei","Produsul se livreaza...",sep="\n")
+# elif optiune == 2:
+#     print(f"Veti primi restul de {bani- Espresso} lei", "Produsul se livreaza...", sep="\n")
+# else:
+#     print("Alegere gresita")
 
-    print("Vei primi restul de:",bani- pret1 )
-elif optiuni== var2 and bani == (ban or ban1):
 
-    print("Vei primi restul de:",bani- pret2 )
-# elif optiuni != (var1 and var2):
-#     print("Alegere gresita!")
+#Problema 4
+counter = 0
+while input("Introduceti parola") != "7788":
+    print("Parola gresita. Mai incercati.")
+    counter += 1
+if counter == 3:
+    break
 else:
-    print('Alegere gresita!')
+    print("Acces permis.")
 
-#Problema3a
 
-print("1. Cappuccino     ... 4 lei","2. Espresso       ... 3.5 lei",sep="\n")
-Cappucino = 4
-Espresso = 3.5
-optiune = 0
-while not optiune:
-    optiune = int(input("Ce optiune alegeti? [1,2]: "))
-    if optiune == 1:
-        print("Ati ales optiunea 1")
-    elif optiune == 2:
-        print("Ati ales optiunea 2")
-    else:
-        print("Alegere gresita")
-        optiune = 0
 
-while True:
-    bani = int(input("Introduceti o banconta [5,10]: "))
-    if bani == 5:
-        print("Ati introdus 5 lei")
-        break
-    elif bani == 10:
-        print("Ati introdus 10 lei")
-        break
-    else:
-        print("Se permit doar bancnote de 5 sau 10 lei")
-        continue
-
-if optiune == 1:
-    print(f"Veti primi restul de {bani-Cappucino} lei","Produsul se livreaza...",sep="\n")
-elif optiune == 2:
-    print(f"Veti primi restul de {bani- Espresso} lei", "Produsul se livreaza...", sep="\n")
-else:
-    print("Alegere gresita")
