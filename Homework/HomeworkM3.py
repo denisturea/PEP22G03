4acfc4a50a40e304ae6623824a0d8056dbe1d07b
denisturea <tureadenis2@gmail.com>
2022-06-20 14:55:32 +0300

    incomplet

---
+++ Homework/HomeworkM3.py
@@ -0,0 +1,34 @@
+# Problema 1:
+# lista = ['abc',123,'1',1]
+# counteri=0
+# counterf=0
+# counterlis=0
+# counters= 0
+# for obiect in lista:
+#     print(f"Tipul obiectului {obiect} este {type(obiect).__name__}. ")
+#
+#     if type(obiect) == int:
+#         counteri += 1
+#     elif type(obiect) == str:
+#         counters +=1
+#
+#     elif type(obiect) == float:
+#         counterf += 1
+#     elif type(obiect)== list:
+#         counterlis +=1
+# print(f"Sunt {counteri} obiecte de timp integer.")
+# print(f"Sunt {counters} obiecte de tip string.")
+# print(f"Sunt {counterf} obiecte de tip float.")
+# print(f"Sunt {counterlis} obiecte de tip lista.")
+
+#Problema 2
+
+sir = str.lower(input("Alege-ti un sir:"))
+list =sir.split()
+next_obj=list.__iter__()
+print(list)
+counter = 0
+for vocala in list:
+    if vocala == "a" :
+        counter +=1
+print("Numarul vocalelor din lista este:", counter)
