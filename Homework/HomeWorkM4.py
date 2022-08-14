d8abb36fbb5452b67349e8a36b56b2deff965667
denisturea <tureadenis2@gmail.com>
2022-08-14 11:09:42 +0300

    Complet

--- Homework/HomeWorkM4.py
+++ Homework/HomeWorkM4.py
@@ -12,28 +12,28 @@
 #
 # #Ex:2
 
-nr = input("Cati carti doriti sa adaugati in biblioteca?: \n ",  )
-lista_carti = []
-for i in range(int(nr)):
-    nume_carte = input("Numele cartii:")
-    nume_autor = input("Numele autorului:")
-    anul_publicarii = input("Anul publicarii:")
-    dict = {'nume':nume_carte, 'autor':nume_autor, 'an':anul_publicarii}
-    lista_carti.append(dict)
-
-print("Cartile dvs sunt:")
-for i in range(len(lista_carti)):
-    print(lista_carti[i])
-# #Ex:3
-
-publicatie = input("Introduce-ti anul: \n")
-carti = []
-carti_selectate = []
-for x in range(len(lista_carti)):
-    my_dict = lista_carti[x]
-    if my_dict['an'] > publicatie:
-        carti.append(my_dict['nume'])
-for y in range(len(carti)):
-    carti_selectate.append(carti[y])
-for carte in carti_selectate:
-    print(f"{carte} a fost publicat dupa {publicatie}")
+# nr = input("Cati carti doriti sa adaugati in biblioteca?: \n ",  )
+# lista_carti = []
+# for i in range(int(nr)):
+#     nume_carte = input("Numele cartii:")
+#     nume_autor = input("Numele autorului:")
+#     anul_publicarii = input("Anul publicarii:")
+#     dict = {'nume':nume_carte, 'autor':nume_autor, 'an':anul_publicarii}
+#     lista_carti.append(dict)
+#
+# print("Cartile dvs sunt:")
+# for i in range(len(lista_carti)):
+#     print(lista_carti[i])
+# # #Ex:3
+#
+# publicatie = input("Introduce-ti anul: \n")
+# carti = []
+# carti_selectate = []
+# for x in range(len(lista_carti)):
+#     my_dict = lista_carti[x]
+#     if my_dict['an'] > publicatie:
+#         carti.append(my_dict['nume'])
+# for y in range(len(carti)):
+#     carti_selectate.append(carti[y])
+# for carte in carti_selectate:
+#     print(f"{carte} a fost publicat dupa {publicatie}")
